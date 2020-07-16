f711c7312ca59fb8b9a7e3ef2ea35dd80bab2dd0
arsikhan <arsalan@local.com>
2020-07-16 12:01:22 +0500

    fetch instance ids

--- main.py
+++ main.py
@@ -10,4 +10,15 @@ load_dotenv()
 print('Starting.....')
 print ()
 bs = boto3.client('elasticbeanstalk')
+ec2 = boto3.client('ec2')
+
+def get_instanceids(environment):
+    print('Getting the instance ids of ', environment)
+    response = bs.describe_environment_resources(EnvironmentName=environment)
+    temp = response['EnvironmentResources']['Instances']
+    return [inst['Id'] for inst in temp]
+
+# def get_instanceips(ids):
+#     print("Getting the ip addresses of the instances")
+#     response = 
 
