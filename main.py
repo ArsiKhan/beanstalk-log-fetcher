3c96915b491433f2e33dd584cff71503b19c9019
arsikhan <arsalan@local.com>
2020-07-16 12:44:41 +0500

    add getinstanceips function

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ load_dotenv()
 print('Starting.....')
 print ()
 bs = boto3.client('elasticbeanstalk')
-ec2 = boto3.client('ec2')
+ec2 = boto3.resource('ec2')
 
 def get_instanceids(environment):
     print('Getting the instance ids of ', environment)
@@ -18,7 +18,14 @@ def get_instanceids(environment):
     temp = response['EnvironmentResources']['Instances']
     return [inst['Id'] for inst in temp]
 
-# def get_instanceips(ids):
-#     print("Getting the ip addresses of the instances")
-#     response = 
+def get_instanceips(ids):
+    print("Getting the ip addresses of the instances")
+    instances = ec2.instances.filter(Filters=[{
+        'Name': 'instance-id',
+        'Values': ids
+    }])
+    return [instance.public_ip_address for instance in instances]
+
+# a = get_instanceids('development')
+# print(get_instanceips(a))
 
